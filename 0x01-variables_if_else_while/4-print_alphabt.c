246df8770e6a1b258d2d0fcd32de7acf7230b9c0
msajiddah <msajiddah@gmail.com>
2022-09-09 19:36:21 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	if (alphabet == 'q')
+		continue;
+
+	else if (alphabet == 'e')
+		continue;
+
+putchar (alphabet);
+}
+
+putchar ('\n');
+
+return (0);
+}
