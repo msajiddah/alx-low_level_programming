356fbc49556a79d530941fd7ce6280eb7c6b6aa0
msajiddah <msajiddah@gmail.com>
2022-09-09 21:26:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * return: Always 0 (success)
+ */
+int main(void)
+{int a;
+long int b;
+long int c;
+char d;
+float f;
+
+printf("size of a char: %1u byte(s)\n", (unsigned long)size of(d));
+printf("size of an int: %1u byte(s)\n", (unsigned long)size of(a));
+printf("size of a long int: %1u byte(s)\n", (unsigned long)size of(b));
+printf("size of a long long int: %1u byte(s)\n", (unsigned long)size of(d));
+printf("size of a float: %1u byte(s)\n", (unsigned long)size of(f));
+return (0);
+}
