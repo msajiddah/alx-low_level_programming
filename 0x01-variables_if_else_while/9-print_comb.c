9cabb77b8992a779c0a962e452f60a73d4208ea3
msajiddah <msajiddah@gmail.com>
2022-09-09 22:51:03 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)/correct)
+ */
+
 int main(void)
 
 {
 	int number;
 
-	for (number=48; number <=57; number++)
+	for (number= 48; number <= 57; number++)
 	{
 		putchar(number);
 
-		if (number==57)
+		if (number== 57)
 
 			break;
 	}
