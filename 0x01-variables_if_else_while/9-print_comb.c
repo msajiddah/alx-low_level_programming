6dc9a9d85e0d14ecc8c034b1725572810763d79c
msajiddah <msajiddah@gmail.com>
2022-09-09 20:14:15 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+
+		if (number == 57)
+
+			break;
+	}
+	putchar(',');
+	putchar (' ');
+}
+putcahr('\n');
+
+return (0);
+
+}
