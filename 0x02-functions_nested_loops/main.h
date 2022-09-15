fc35918246ab6aa85f7e31542f5965aa89a3d448
msajiddah <msajiddah@gmail.com>
2022-09-14 20:00:49 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int _print_sign(int n);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
@@ -16,3 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
+#endif
