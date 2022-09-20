cda7ba278014d909e6818b5942190c8b3d8e10ba
msajiddah <msajiddah@gmail.com>
2022-09-19 17:08:01 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* strcpy - copy paste string
+* @dest: destination
+* @src: source
+*
+* Return: destination
+*/
+
+char *_strcpy(char *dest, char *src);
+
+{
+int inc = 0;
+
+while (*(src + inc) != '\10')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\10';
+
+return (dest);
+}
+
